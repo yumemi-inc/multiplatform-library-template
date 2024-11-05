ddae764d0d1e85eebae6ced1eb41b54b63dbacde
Hiroyuki Kusu <h_kusu@yumemi.co.jp>
2024-11-05 18:12:23 +0900

    Customize for us

--- library/src/commonTest/kotlin/io/yumemi/something/FibiTest.kt
+++ library/src/commonTest/kotlin/io/yumemi/something/FibiTest.kt
@@ -1,4 +1,4 @@
-package io.github.kotlin.fibonacci
+package io.yumemi.something
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
---
+++ library/src/iosMain/kotlin/io/yumemi/something/fibiprops.ios.kt
@@ -0,0 +1,4 @@
+package io.yumemi.something
+
+actual val firstElement: Int = 3
+actual val secondElement: Int = 4
